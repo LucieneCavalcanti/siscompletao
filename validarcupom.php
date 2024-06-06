02e83debcd725f779df540bcc951b16bf2d8c179
Professora Luciene Cavalcanti <lucienecavalcanti@live.com>
2024-06-05 22:23:34 -0300

    06-06-2024
    Ainda falta validar o cupom e criar as sessions

--- validarcupom.php
+++ validarcupom.php
@@ -8,13 +8,49 @@ if(isset($_GET['cupom'])){
     foreach($resultado as $cupom) {
         if ($cupom['id']>0){
             echo "<p> Cupom ok</p>";
+            session_start();
+            if(is_null($cupom['quantidadeUso'])){
+                if(is_null($cupom['valorMinimoPedido'])){
+                    $_SESSION['cupom']=$cupom['cupom'];
+                    $_SESSION['idcupom']=$cupom['id'];
+                    $_SESSION['valordesconto'] = $cupom['valor'];
+                    //  
+                }else{
+                    if($cupom['valorMinimo']<=$valorPedido){
+                        $_SESSION['cupom']=$cupom['cupom'];
+                        $_SESSION['idcupom']=$cupom['id'];
+                        $_SESSION['valordesconto'] = $cupom['valor']; 
+                        //
+                    }else{
+                        header("location:carrinho.php?cupom=".$cupom['cupom']."&validacao=compremais");
+                    }
+                }
+                
+            }else if($cupom['quantidadeUso']<=$cupom['quantidadeUtilizada']){
+                header("location:carrinho.php?cupom=".$cupom['cupom']."&validacao=limiteexcedido");
+            }else{
+                if($cupom['valorMinimo']<=$valorPedido){
+                    $_SESSION['cupom']=$cupom['cupom'];
+                    $_SESSION['idcupom']=$cupom['id'];
+                    $_SESSION['valordesconto'] = $cupom['valor']; 
+                    //atualizar a quantidade de cupons disponíveis
+                    //
+                }else{
+                    header("location:carrinho.php?cupom=".$cupom['cupom']."&validacao=compremais");
+                }
+            } 
+            header("location:carrinho.php?cupom=".$cupom['cupom']."&validacao=ok");
             exit;
         }else {
             echo "<p> Cupom inválido</p>";
+            header("location:carrinho.php?cupom=".$cupom['cupom']."&validacao=negativo");
+
             exit;    
         }
     }
     echo "<p> Cupom inválido</p>";
+    header("location:carrinho.php?cupom=".$cupom."&validacao=negativo");
+
     exit;
 }else {
     echo "<p> Erro</p>";
